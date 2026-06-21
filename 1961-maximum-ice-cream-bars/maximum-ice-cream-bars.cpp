d753e5698ae5c7374ee243165c1f5ec4e1566aef
Avinash Shetty <abhiapril122005@gmail.com>
2026-06-21 17:59:34 +0530

    Time: 475 ms (5.38%) | Memory: 80.3 MB (37.51%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -3,13 +3,16 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(),costs.end());
         int count=0;
-   
+        int sum=0;
+       
+        if(costs[0]>coins)return 0;
         for(int i=0;i<costs.size();i++){
-            if(coins>=costs[i]){
-                coins-=costs[i];
-                count++;
-            }
-            else{
+            if(sum+costs[i]<=coins){
+           
+            sum+=costs[i];
+            cout<<sum<<"\t"<<count<<endl;
+            count++;
+            }else{
                 break;
             }
         }
