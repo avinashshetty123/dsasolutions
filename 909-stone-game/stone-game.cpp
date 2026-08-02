1a1dcae2c8f8c7a6e7d8e8704f3a404714ec89ba
Avinash Shetty <abhiapril122005@gmail.com>
2026-08-02 21:18:29 +0530

    Time: 10 ms (28.51%) | Memory: 19.7 MB (20.08%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i < n - l; i++) {
+                int j = i + l - 1;
+                int pickL = nums[i] - dp[i + 1][j];
+                int pickR = nums[j] - dp[i][j - 1];
+                dp[i][j] = max(pickR, pickL);
+            }
+        }
+        return dp[0][n - 1] >= 0;
+    }
+};
