3e91140bca3b5ffec7058d37b49bd1941393f4c9
Avinash Shetty <abhiapril122005@gmail.com>
2026-09-07 19:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (90.96%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    static constexpr int MOD=1e9+7;
+public:
+    int distinctSubseqII(string s) {
+        int total=0,dp[26]{};
+        for(auto &c:s){
+            c-='a';
+            int add=(total-dp[c]+MOD)%MOD;
+            dp[c]=1+total;
+            total=(dp[c]+add)%MOD;
+        }
+        return total;
+        
+    }
+};
