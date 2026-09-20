7ac18360d3ccda8e6bd7f0e821ec4c10e89db93e
Avinash Shetty <abhiapril122005@gmail.com>
2026-09-20 19:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            int s1=26-(s[i]-'a');
+            ans+=(s1*(i+1));
+        }
+
+        return ans;
+    }
+};
